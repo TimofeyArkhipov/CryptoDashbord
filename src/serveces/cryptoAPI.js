c4acc27c744a3f0612c846c60e5c208b8dddd5d5
Timofey Arkhipov <54080963+TimofeyArkhipov@users.noreply.github.com>
2022-07-04 17:19:59 +0300

    Add files via upload
    add crypto detaild and homepage

---
+++ src/serveces/cryptoAPI.js
@@ -0,0 +1,27 @@
+
+import {createApi, fetchBaseQuery}  from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '0f09a0217cmshcf05d1bdf7f2291p1eb954jsn71c7bf4e00cf',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl =  'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi =  createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
+        })
+    })
+})
+
+
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery} = cryptoApi;
